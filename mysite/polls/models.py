f23c24a9534422be89384cd7052672a31f2286ec
Allan Ma <allan_ma@outlook.com>
2025-03-07 19:22:48 -0500

    Fix Black formatting issues

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,7 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -12,7 +13,7 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-    
+
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
